804c3a4bfda8647353fec45cc746a6b45bb3347a
PedroRevesMD <pedroreves11@gmail.com>
2025-12-30 20:17:15 -0300

    feat(html): add initial implementation of CallSection's html

---
+++ src/components/CallSection.tsx
@@ -0,0 +1,16 @@
+import "../index.css"
+
+export default function CallSection() {
+  return (
+    <section>
+      <div>
+        <h3>Let’s make things happen</h3>
+        <p>Contact us today to learn more about how our digital marketing services can help your business grow and succeed online.</p>
+        <a href="/">Get your free proposal</a>
+      </div>
+      <div>
+        <img src="" alt="Abstract Illustration" />
+      </div>
+    </section>
+  )
+}
