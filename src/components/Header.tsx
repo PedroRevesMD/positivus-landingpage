4803d3c00a462c281317cbb993c11c8f498a8c5f
PedroRevesMD <pedroreves11@gmail.com>
2025-12-30 20:00:06 -0300

    feat(html): add initial implementation of header's html

---
+++ src/components/Header.tsx
@@ -0,0 +1,18 @@
+export default function Heade() {
+  return (
+    <header>
+      <img src="" alt="Positivus's Logo" />
+      <nav>
+        <ul>
+          <li><a href="/">About us</a></li>
+          <li><a href="/">Services</a></li>
+          <li><a href="/">Use Cases</a></li>
+          <li><a href="/">Pricing</a></li>
+          <li><a href="/">Blog</a></li>
+        </ul>
+        <a href="/">Request a Quote</a>
+      </nav>
+      <button type="button">Menu</button>
+    </header>
+  )
+}
