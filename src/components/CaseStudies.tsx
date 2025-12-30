1954db4de9ce2e732d319e8df776fe508c981cd3
PedroRevesMD <pedroreves11@gmail.com>
2025-12-30 20:27:31 -0300

    feat(html): initial implementation of CaseStudies html

---
+++ src/components/CaseStudies.tsx
@@ -0,0 +1,24 @@
+import "../index.css"
+
+export default function CaseStudies() {
+  return (
+    <section>
+      <h3>Case Studies</h3>
+      <p>Explore Real-Life Examples of Our Proven Digital Marketing Success through Our Case Studies</p>
+      <div>
+        <article>
+          <p>For a local restaurant, we implemented a targeted PPC campaign that resulted in a 50% increase in website traffic and a 25% increase in sales.</p>
+          <a href="/">Learn More</a>
+        </article>
+        <article>
+          <p>For a B2B software company, we developed an SEO strategy that resulted in a first page ranking for key keywords and a 200% increase in organic traffic.</p>
+          <a href="/">Learn More</a>
+        </article>
+        <article>
+          <p>For a national retail chain, we created a social media marketing campaign that increased followers by 25% and generated a 20% increase in online sales.</p>
+          <a href="/">Learn More</a>
+        </article>
+      </div>
+    </section>
+  )
+}
