8f77a3ec704146d7dd95804012377e4a565bc43e
PedroRevesMD <pedroreves11@gmail.com>
2025-12-30 20:00:28 -0300

    feat(html): add initial implementation of hero's html

---
+++ src/components/Hero.tsx
@@ -0,0 +1,25 @@
+import "../index.css"
+export default function Hero() {
+  return (
+    <section>
+      <div>
+        <div>
+          <h1>Navigating the digital landscape for success</h1>
+          <p>Our digital marketing agency helps businesses grow and succeed online through a range of services including SEO, PPC, social media marketing, and content creation.</p>
+          <a href="/">Book a consultation</a>
+        </div>
+        <div>
+          <img src="" alt="A marketing illustration" />
+        </div>
+      </div>
+      <div>
+        <img src="" alt="Amazon's Logo" />
+        <img src="" alt="Dribbble's Logo" />
+        <img src="" alt="Hubspot's Logo" />
+        <img src="" alt="Notion Logo" />
+        <img src="" alt="Netflix Logo" />
+        <img src="" alt="Zoom Logo" />
+      </div>
+    </section>
+  )
+}
