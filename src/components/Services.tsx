e6f43e856dbdd3d0490fdfe811d9289abdc68ee8
PedroRevesMD <pedroreves11@gmail.com>
2025-12-30 20:01:09 -0300

    feat(html): add initial implementation of services html

---
+++ src/components/Services.tsx
@@ -0,0 +1,27 @@
+import "../index.css"
+export default function Services() {
+  return (
+    <section>
+      <h3>Services</h3>
+      <p>At our digital marketing agency, we offer a range of services to help businesses grow and succeed online. These services include:</p>
+      <article>
+        <h4>Search engine optimization</h4>
+      </article>
+      <article>
+        <h4>Pay-per-click advertising</h4>
+      </article>
+      <article>
+        <h4>Social Media Marketing</h4>
+      </article>
+      <article>
+        <h4>Email Marketing</h4>
+      </article>
+      <article>
+        <h4>Content Creation</h4>
+      </article>
+      <article>
+        <h4>Analytics and Tracking</h4>
+      </article>
+    </section>
+  )
+}
