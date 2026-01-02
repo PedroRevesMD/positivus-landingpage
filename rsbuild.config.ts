5c938af948627f98a71a149ad98f44cbd61225a7
PedroRevesMD <pedroreves11@gmail.com>
2026-01-02 20:51:49 -0300

    chore: add space grotesk font

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -8,6 +8,30 @@ export default defineConfig({
     title: "Positivus | Home",
     meta: {
       description: "A Landing page for a company called positivus",
-    }
+    },
+    tags: [
+      {
+        tag: 'link',
+        attrs: {
+          rel: 'preconnect',
+          href: 'https://fonts.googleapis.com',
+        },
+      },
+      {
+        tag: 'link',
+        attrs: {
+          rel: 'preconnect',
+          href: 'https://fonts.gstatic.com',
+          crossorigin: true,
+        },
+      },
+      {
+        tag: 'link',
+        attrs: {
+          rel: 'stylesheet',
+          href: 'https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300..700&display=swap',
+        },
+      },
+    ],
   }
 });
