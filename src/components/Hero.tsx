b86b979cfdf1c0c09b8bdb25deb331e7c3792146
PedroRevesMD <pedroreves11@gmail.com>
2026-01-05 21:46:45 -0300

    Abstraction of Hardcoded values

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,24 +1,20 @@
 import "../index.css"
+import HeroIllustration from "../assets/hero-illustration.svg"
 export default function Hero() {
+  // TODO: Change Hardcoded font values
+  // FIX:  Margin of the "Book a Consultation" Button
+  // TODO: Add Responsiviness (Menu Button and Nav Links, Logo Image)
   return (
-    <section>
-      <div>
-        <div>
-          <h1>Navigating the digital landscape for success</h1>
-          <p>Our digital marketing agency helps businesses grow and succeed online through a range of services including SEO, PPC, social media marketing, and content creation.</p>
-          <a href="/">Book a consultation</a>
+    <section className="my-12">
+      <div className="flex flex-col-reverse">
+        <div className="text-center mx-8">
+          <h1 className="font-[Space_Grotesk] font-bold text-4xl">Navigating the digital landscape for success</h1>
+          <p className="font-[Space_Grotesk]">Our digital marketing agency helps businesses grow and succeed online through a range of services including SEO, PPC, social media marketing, and content creation.</p>
+          <a href="/" className="font-[Space_Grotesk] bg-[#191A23] text-white rounded-lg px-6 py-4 my-8">Book a consultation</a>
         </div>
-        <div>
-          <img src="" alt="A marketing illustration" />
+        <div className="mx-auto">
+          <img src={HeroIllustration} alt="A marketing illustration" />
         </div>
-      </div>
-      <div>
-        <img src="" alt="Amazon's Logo" />
-        <img src="" alt="Dribbble's Logo" />
-        <img src="" alt="Hubspot's Logo" />
-        <img src="" alt="Notion Logo" />
-        <img src="" alt="Netflix Logo" />
-        <img src="" alt="Zoom Logo" />
       </div>
     </section>
   )
