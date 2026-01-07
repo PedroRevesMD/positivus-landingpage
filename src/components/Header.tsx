ec16f0c07c609a34908c150d6a525f9fbe140513
PedroRevesMD <pedroreves11@gmail.com>
2026-01-06 21:21:38 -0300

    fix: remove hardcoded font and color values

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,19 +1,22 @@
 import "../index.css"
+import logo from "../assets/logo.svg"
 export default function Header() {
+  // TODO: Add Responsiviness (Menu Button and Nav Links, Logo Image)
+  // FIX: Border Bug on "Menu Button" and "Request a Quote" Button
   return (
-    <header>
-      <img src="" alt="Positivus's Logo" />
-      <nav>
-        <ul>
+    <header className="flex justify-between mt-16 mx-20 font-grotesk mb-30 items-center">
+      <img src={logo} alt="Positivus's Logo" className="size-40" />
+      <nav className="hidden md:flex items-center">
+        <ul className="flex gap-x-8 text-lg">
           <li><a href="/">About us</a></li>
           <li><a href="/">Services</a></li>
           <li><a href="/">Use Cases</a></li>
           <li><a href="/">Pricing</a></li>
           <li><a href="/">Blog</a></li>
         </ul>
-        <a href="/">Request a Quote</a>
+        <a href="/" className="border-dark-gray border-x text-xl rounded-xl py-2 px-4">Request a Quote</a>
       </nav>
-      <button type="button">Menu</button>
+      <button type="button" className="cursor-pointer md:hidden font-grotesk borderd-dark-gray border-solid">Menu</button>
     </header>
   )
 }
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,16 +1,15 @@
 import "../index.css"
 import HeroIllustration from "../assets/hero-illustration.svg"
 export default function Hero() {
-  // TODO: Change Hardcoded font values
   // FIX:  Margin of the "Book a Consultation" Button
   // TODO: Add Responsiviness (Menu Button and Nav Links, Logo Image)
   return (
     <section className="my-12">
       <div className="flex flex-col-reverse">
         <div className="text-center mx-8">
-          <h1 className="font-[Space_Grotesk] font-bold text-4xl">Navigating the digital landscape for success</h1>
-          <p className="font-[Space_Grotesk]">Our digital marketing agency helps businesses grow and succeed online through a range of services including SEO, PPC, social media marketing, and content creation.</p>
-          <a href="/" className="font-[Space_Grotesk] bg-[#191A23] text-white rounded-lg px-6 py-4 my-8">Book a consultation</a>
+          <h1 className="font-grotesk font-bold text-4xl">Navigating the digital landscape for success</h1>
+          <p className="font-grotesk">Our digital marketing agency helps businesses grow and succeed online through a range of services including SEO, PPC, social media marketing, and content creation.</p>
+          <a href="/" className="font-grotesk bg-dark-gray text-white rounded-lg px-6 py-4 my-8">Book a consultation</a>
         </div>
         <div className="mx-auto">
           <img src={HeroIllustration} alt="A marketing illustration" />
