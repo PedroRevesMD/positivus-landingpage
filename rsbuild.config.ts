107d986e5d89cd7a6ab1157d3b15af83fb087cfa
PedroRevesMD <pedroreves11@gmail.com>
2026-01-11 16:54:22 -0300

    fix: increase performance and adding a lang to the website

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -6,33 +6,10 @@ export default defineConfig({
   plugins: [pluginReact()],
   html: {
     title: "Positivus | Home",
+    template: "./index.html",
     favicon: "./public/favicon.ico",
     meta: {
       description: "A Landing page for a company called positivus",
     },
-    tags: [
-      {
-        tag: 'link',
-        attrs: {
-          rel: 'preconnect',
-          href: 'https://fonts.googleapis.com',
-        },
-      },
-      {
-        tag: 'link',
-        attrs: {
-          rel: 'preconnect',
-          href: 'https://fonts.gstatic.com',
-          crossorigin: true,
-        },
-      },
-      {
-        tag: 'link',
-        attrs: {
-          rel: 'stylesheet',
-          href: 'https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300..700&display=swap',
-        },
-      },
-    ],
   }
 });
--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+import "@fontsource/space-grotesk/300.css";
+import "@fontsource/space-grotesk/400.css";
+import "@fontsource/space-grotesk/500.css";
+import "@fontsource/space-grotesk/600.css";
+import "@fontsource/space-grotesk/700.css";
+
 const rootEl = document.getElementById('root');
 if (rootEl) {
   const root = ReactDOM.createRoot(rootEl);
