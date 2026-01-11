76f41273cf11bcf84eade6bb980f0562b0051325
PedroRevesMD <pedroreves11@gmail.com>
2026-01-11 14:12:54 -0300

    feat: create Hero component

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -6,7 +6,7 @@ export default function Hero() {
       <div className="flex flex-col gap-y-4 text-center lg:text-left items-center lg:items-start my-4 lg:max-w-[500px]">
         <h1 className="font-semibold font-grotesk text-5xl">Navigating the digital landscape for success</h1>
         <p className="font-normal font-grotesk text-md">Our digital marketing agency helps businesses grow and succeed online through a range of services including SEO, PPC, social media marketing, and content creation.</p>
-        <a href="/" className="my-4 bg-dark-gray font-grotesk text-white w-fit px-8 py-4 rounded-lg ">Book a consultation</a>
+        <a href="/" className="my-4 bg-dark-gray font-grotesk text-white w-fit px-8 py-4 rounded-lg">Book a consultation</a>
       </div>
       <div className="mx-auto -mr-2">
         <img src={HeroIllustration} alt="A marketing illustration" />
