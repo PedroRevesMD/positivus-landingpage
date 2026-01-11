010003dde59d57b5e9c986f01036ddb6b557c754
PedroRevesMD <pedroreves11@gmail.com>
2026-01-11 14:10:13 -0300

    feat: create Service Component

---
+++ src/components/ServiceCard.tsx
@@ -0,0 +1,57 @@
+type VariantType = 'green' | 'dark' | 'grey';
+
+interface ServiceCardProps {
+  cardName: string;
+  imgUrlPath: string;
+  variant: VariantType;
+}
+
+interface CardStyles {
+  cardBg: string,
+  textBg: string,
+  arrowCircle: string,
+  buttonText: string,
+  arrowColor: string,
+}
+
+export default function ServiceCard({ cardName, imgUrlPath, variant = "grey" }: ServiceCardProps) {
+  const variants: Record<VariantType, CardStyles> = {
+    grey: {
+      cardBg: "bg-light-gray",
+      textBg: "bg-lime-green",
+      arrowCircle: "bg-dark-gray",
+      buttonText: "text-dark-gray",
+      arrowColor: "#b9ff66",
+    },
+    dark: {
+      cardBg: "bg-dark-gray",
+      textBg: "bg-white",
+      arrowCircle: "bg-light-gray",
+      buttonText: "text-light-gray",
+      arrowColor: "#191A23",
+    },
+    green: {
+      cardBg: "bg-lime-green",
+      textBg: "bg-white",
+      arrowCircle: "bg-light-gray",
+      buttonText: "text-dark-gray",
+      arrowColor: "#191A23",
+    },
+  };
+
+  const currentTheme = variants[variant] || variants.grey;
+
+  return (
+    <article className={`${currentTheme.cardBg} rounded-4xl shadow-[0px_5px_0px_0px_rgba(25,_26,_35,_1)] max-w-2xl flex gap-x-20 items-center mb-8 p-12 border border-solid border-dark-gray`}>
+      <div className="flex flex-col gap-y-16">
+        <h4 className={`${currentTheme.textBg} font-semibold text-md lg:text-2xl font-grotesk p-1.5 rounded-sm text-dark-gray`}>{cardName}</h4>
+        <div className="flex gap-x-4 items-center">
+          <span className={`${currentTheme.buttonText} font-grotesk cursor-pointer font-medium`}>Learn More</span>
+        </div>
+      </div>
+      <div className="hidden lg:flex items-center justify-end max-w-[40%]">
+        <img src={imgUrlPath} alt="An Illustration" className="hidden lg:inline" />
+      </div>
+    </article>
+  )
+}
--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -1,27 +1,29 @@
 import "../index.css"
+import EngineIllustration from "../assets/engine-illustration.svg"
+import PayPerClickIllustration from "../assets/pay-click.svg"
+import SocialMediaMarketingIllustration from "../assets/social-media.svg"
+import EmailMarketingIllustration from "../assets/email-marketing.svg"
+import ContentCreationIllustration from "../assets/content-creation.svg"
+import AnalyticsAndTrackingIllustration from "../assets/analytics-tracking.svg"
+import ServiceCard from "./ServiceCard"
+// FIX: Arrumar Tamanho dos Cards (alguns estão com menos tamanho do que outros)
+// FIX: Adicionar a Arrow em conjunto com a div "Learn More"
+// TODO: Abstrair os valores hardcoded do título  
 export default function Services() {
   return (
-    <section>
-      <h3>Services</h3>
-      <p>At our digital marketing agency, we offer a range of services to help businesses grow and succeed online. These services include:</p>
-      <article>
-        <h4>Search engine optimization</h4>
-      </article>
-      <article>
-        <h4>Pay-per-click advertising</h4>
-      </article>
-      <article>
-        <h4>Social Media Marketing</h4>
-      </article>
-      <article>
-        <h4>Email Marketing</h4>
-      </article>
-      <article>
-        <h4>Content Creation</h4>
-      </article>
-      <article>
-        <h4>Analytics and Tracking</h4>
-      </article>
+    <section className="max-w-screen m-8 lg:m-20">
+      <div className="flex flex-col lg:flex-row gap-x-8 items-center">
+        <h3 className="font-medium bg-lime-green p-2 rounded-xl text-dark-gray text-2xl font-grotesk">Services</h3>
+        <p className="text-base font-grotesk max-w-[520px] my-4 text-center lg:text-start">At our digital marketing agency, we offer a range of services to help businesses grow and succeed online. These services include:</p>
+      </div>
+      <div className="grid grid-cols-1 lg:grid-cols-2 my-12">
+        <ServiceCard cardName="Search engine optimization" imgUrlPath={EngineIllustration} variant="grey" />
+        <ServiceCard cardName="Pay-per-click advertising" imgUrlPath={PayPerClickIllustration} variant="green" />
+        <ServiceCard cardName="Social Media Marketing" imgUrlPath={SocialMediaMarketingIllustration} variant="dark" />
+        <ServiceCard cardName="Email Marketing" imgUrlPath={EmailMarketingIllustration} variant="grey" />
+        <ServiceCard cardName="Content Creation" imgUrlPath={ContentCreationIllustration} variant="green" />
+        <ServiceCard cardName="Analytics and Tracking" imgUrlPath={AnalyticsAndTrackingIllustration} variant="dark" />
+      </div>
     </section>
   )
 }
