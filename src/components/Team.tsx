359f8998196a151cbb39069dcb6e10ee6369b650
PedroRevesMD <pedroreves11@gmail.com>
2026-01-11 14:09:12 -0300

    feat: create Team component

--- src/components/Team.tsx
+++ src/components/Team.tsx
@@ -56,18 +56,18 @@ export const teamMembers: TeamCardType[] = [
 
 export default function Team() {
   return (
-    <section className="max-w-screen m-20">
+    <section className="max-w-screen m-8 lg:m-20">
       <div className="flex flex-col lg:flex-row gap-x-8 items-center">
         <h3 className="font-medium bg-lime-green p-2 rounded-xl text-dark-gray text-2xl font-grotesk">Team</h3>
         <p className="text-base font-grotesk max-w-[450px] my-4 text-center lg:text-start">Meet the skilled and experienced team behind our successful digital marketing strategies</p>
       </div>
-      <ul className="grid grid-cols-1 lg:grid-cols-3 my-6">
+      <ul className="grid grid-cols-1 lg:grid-cols-3 my-6 justify-items-center">
         {teamMembers.map((member) => (
           <TeamCard id={member.id} imgUrlPath={member.imgUrlPath} name={member.name} position={member.position} description={member.description} />
         ))}
       </ul>
-      <div className="flex justify-center lg:justify-end lg:mr-26 max-w-screen my-4">
-        <button type="button" className="bg-dark-gray font-grotesk text-white w-fit px-16 py-4 my-4 rounded-lg" >See all team</button>
+      <div className="flex justify-center lg:justify-end lg:mr-16 max-w-screen my-4">
+        <button type="button" className="bg-dark-gray font-grotesk text-white w-fit px-16 py-4 my-4 rounded-lg cursor-pointer" >See all team</button>
       </div>
     </section >
   )
---
+++ src/types/TeamCardType.ts
@@ -0,0 +1,7 @@
+export interface TeamCardType {
+  id: string,
+  imgUrlPath: string,
+  name: string,
+  position: string,
+  description: string,
+}
