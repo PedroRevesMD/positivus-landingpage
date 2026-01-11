ccdf5e1e891907ee0a86dccf44fffd39013e14fa
PedroRevesMD <pedroreves11@gmail.com>
2026-01-10 23:40:08 -0300

    feat: create hero component

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,19 +1,15 @@
 import "../index.css"
 import HeroIllustration from "../assets/hero-illustration.svg"
 export default function Hero() {
-  // FIX:  Margin of the "Book a Consultation" Button
-  // TODO: Add Responsiviness (Menu Button and Nav Links, Logo Image)
   return (
-    <section className="my-12">
-      <div className="flex flex-col-reverse">
-        <div className="text-center mx-8">
-          <h1 className="font-grotesk font-bold text-4xl">Navigating the digital landscape for success</h1>
-          <p className="font-grotesk">Our digital marketing agency helps businesses grow and succeed online through a range of services including SEO, PPC, social media marketing, and content creation.</p>
-          <a href="/" className="font-grotesk bg-dark-gray text-white rounded-lg px-6 py-4 my-8">Book a consultation</a>
-        </div>
-        <div className="mx-auto">
-          <img src={HeroIllustration} alt="A marketing illustration" />
-        </div>
+    <section className="flex items-center flex-col-reverse my-20 lg:flex-row m-20">
+      <div className="flex flex-col gap-y-4 text-center lg:text-left items-center lg:items-start my-4 lg:max-w-[500px]">
+        <h1 className="font-semibold font-grotesk text-5xl">Navigating the digital landscape for success</h1>
+        <p className="font-normal font-grotesk text-md">Our digital marketing agency helps businesses grow and succeed online through a range of services including SEO, PPC, social media marketing, and content creation.</p>
+        <a href="/" className="my-4 bg-dark-gray font-grotesk text-white w-fit px-8 py-4 rounded-lg ">Book a consultation</a>
+      </div>
+      <div className="mx-auto -mr-2">
+        <img src={HeroIllustration} alt="A marketing illustration" />
       </div>
     </section>
   )
