a4e76a51cb6028e330e355e7b8279e3e97904f4f
PedroRevesMD <pedroreves11@gmail.com>
2026-01-11 12:31:40 -0300

    feat: create CallSection component

--- src/components/CallSection.tsx
+++ src/components/CallSection.tsx
@@ -1,15 +1,17 @@
 import "../index.css"
+import CallSectionIllustration from "../assets/cta-illustration.svg"
+
 
 export default function CallSection() {
   return (
-    <section>
-      <div>
-        <h3>Let’s make things happen</h3>
-        <p>Contact us today to learn more about how our digital marketing services can help your business grow and succeed online.</p>
-        <a href="/">Get your free proposal</a>
+    <section className="bg-light-gray m-20 max-w-screen flex flex-col items-center justify-between lg:flex-row px-12 rounded-2xl">
+      <div className="font-grotesk text-center lg:text-start my-8 max-w-lg flex flex-col gap-y-2">
+        <h3 className="font-semibold text-3xl">Let’s make things happen</h3>
+        <p className="text-base">Contact us today to learn more about how our digital marketing services can help your business grow and succeed online.</p>
+        <a href="/" className="bg-dark-gray font-grotesk text-white w-fit my-4 px-8 py-4 rounded-lg ">Get your free proposal</a>
       </div>
-      <div>
-        <img src="" alt="Abstract Illustration" />
+      <div className="hidden lg:inline mr-45">
+        <img src={CallSectionIllustration} alt="Abstract Illustration" />
       </div>
     </section>
   )
